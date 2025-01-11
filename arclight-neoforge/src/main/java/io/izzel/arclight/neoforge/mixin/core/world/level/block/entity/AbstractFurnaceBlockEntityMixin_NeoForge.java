60b73d0abc170c2f2058c8e4830c58dce321f4c1
JeremGaming <98130953+JeremGamingYT@users.noreply.github.com>
2025-01-11 14:32:26 -0500

    Updating ?

--- arclight-neoforge/src/main/java/io/izzel/arclight/neoforge/mixin/core/world/level/block/entity/AbstractFurnaceBlockEntityMixin_NeoForge.java
+++
@@ -1,9 +0,0 @@
-package io.izzel.arclight.neoforge.mixin.core.world.level.block.entity;
-
-import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(AbstractFurnaceBlockEntity.class)
-public abstract class AbstractFurnaceBlockEntityMixin_NeoForge {
-
-}
